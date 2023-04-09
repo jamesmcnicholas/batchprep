5966c941374a15f22ec2722e2f469b91a5f67dd6
James <james.mcnicholas1@gmail.com>
2023-04-09 01:28:11 +0100

    Dark mode!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'batchprep';
+  faLightbulb = faLightbulb;
+  isDark = false;
+
+  toggleDarkMode(){
+    const cards = document.getElementsByClassName("card");
+    var headers = document.getElementsByTagName("h2");
+    var subheaders = document.getElementsByTagName("h4");
+
+    if(this.isDark) {
+      document.body.style.backgroundColor = "#ffffff"
+      this.isDark = false;
+    } else {
+      document.body.style.backgroundColor = "#21252b"
+      this.isDark = true;
+    }
+
+    Array.prototype.forEach.call(cards, function(el) {  
+      el.classList.toggle("dark-mode");  
+    });
+
+    Array.prototype.forEach.call(headers, function(el) {  
+      el.classList.toggle("dark-mode");  
+    });
+
+    Array.prototype.forEach.call(subheaders, function(el) {  
+      el.classList.toggle("dark-mode");  
+    });
+
+
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { IngredientTypesComponent } from './ingredient/ingredient-types/ingredient-types.component';
+import { AddIngredientComponent } from './add-ingredient/add-ingredient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngredientComponent,
+    IngredientTypesComponent,
+    AddIngredientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
